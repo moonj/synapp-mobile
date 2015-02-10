6cf0e6c97bd3439ecf014e1e8e79f82df7ee033f
Jay Moon <jaymoon@stanford.edu>
2015-02-10 15:50:41 -0800

    lots of stuff

--- js/synapp.js
+++ js/synapp.js
@@ -12,3 +12,31 @@ $("body").click(function() {
   $("body").removeClass("overlay");
   $(".chat").removeClass("faded");
 });
+
+$(function(){
+  var mySwiper = $('.swiper-container').swiper({
+    mode:'horizontal',
+    loop: false,
+    onSlideNext: function() {
+      $("body").addClass("summary");
+      $("#chat").removeClass("active");
+      $("#summary").addClass("active");
+      $(".message-input").css("display", "none");
+    },
+    onSlidePrev: function() {
+      $("body").removeClass("summary");
+      $("#chat").addClass("active");
+      $("#summary").removeClass("active");
+      $(".message-input").css("display", "block");
+    }
+  });
+
+  $("#chat").click(function() {
+    mySwiper.swipePrev();
+  });
+
+  $("#summary").click(function() {
+    mySwiper.swipeNext();
+  });
+
+})        
