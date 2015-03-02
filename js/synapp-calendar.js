6edeaf2440ade22605333a90c2c24e7911674aa1
Jay Moon <jaymoon@stanford.edu>
2015-03-02 06:48:59 -0800

    when2meet simple calendar styling done, added events

--- js/synapp-calendar.js
+++ js/synapp-calendar.js
@@ -1,13 +1,5 @@
-var hammer = new Hammer($(".container")[0]);
-var start, end;
-
-hammer.on("press", function(e) {
-  start = e.center.y;
-  console.log("press start");
-});
-
-hammer.on("pressup", function(e) {
-  end = e.center.y
-  $(".container").prepend("<div class=\"free-time\" style=\"top: " + start + "px; height: " + (Math.abs(start - end)) + "px;");
-  console.log("oh hey there");
+$(document).ready(function() {
+  $("#time-bar").selectable({
+    delay: 100 
+  });
 });
